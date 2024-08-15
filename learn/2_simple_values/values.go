fe125c6da1df36019290f973b92ea2c93fdf8175
S Soumyakanta <ssoumyakanta@fightright.in>
2024-08-15 07:54:07 +0530

    simple values in golang

---
+++ learn/2_simple_values/values.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	//string
+	fmt.Println("s-soumyakanta.com")
+
+	//bool
+	fmt.Println(true)
+
+	//float
+	fmt.Println(10.11 - 0.11)
+
+	//int
+	fmt.Println(22)
+}
