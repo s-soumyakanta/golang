786a5e921bf9893558da7a9d1fcfe66f5bfbc94a
S Soumyakanta <mr.ssoumyakanta@gmail.com>
2024-08-17 21:03:59 +0530

    arrays and slices

---
+++ learn/8_arrays/arrays.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//2d arrays
+	pins := [2][2]int{{753023, 234323}, {234523, 923423}}
+
+	fmt.Println(pins) //[[753023 234323] [234523 923423]]
+
+	//add values to array while declaring array or in single line
+
+	ages := [4]int{3, 4, 2}
+
+	fmt.Println(ages) //[3 4 2 0]
+
+	//In golang arrays are number sequece of specific length -
+	//we define number of element and they will be same type
+
+	//we use arrays when their size is predictable or fixed size
+	//memory optimization
+	//constant time access of elements
+
+	//in int type array by default for each element zero added
+	var nums [5]int
+
+	nums[0] = 33
+	// empty string  added to array
+	var names [3]string
+
+	// falsy value added by default for array of bool
+	var boolVals [4]bool
+	boolVals[3] = true
+	fmt.Println(boolVals) //[false false false true]
+
+	fmt.Println(names) //[ ]
+
+	fmt.Println(nums[0])   //33
+	fmt.Println(nums)      //[33 0 0 0 0]
+	fmt.Println(len(nums)) //5
+
+}
---
+++ learn/9_slices/slices.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func main() {
+	// No need to specify the length of arrays. Dynamic arrays.
+	// Most used construct in go
+	// + useful methods
+
+	//uninitialized slice is nil
+	var nums []int
+	fmt.Println(nums)        //currently it is empty
+	fmt.Println(nums == nil) //true
+	fmt.Println(len(nums))   //0
+
+	var ages = make([]int, 2)
+	//initial capacity is 2
+	fmt.Println(cap(ages)) //2
+	fmt.Println(ages)      // [0 0]
+
+	var pins = make([]int, 2, 5)
+	pins = append(pins, 2)
+	pins = append(pins, 3)
+	pins = append(pins, 4)
+	pins = append(pins, 5)
+	pins = append(pins, 6)
+	pins = append(pins, 7)
+	fmt.Println(pins)      //[0 0 2 3 4 5 6 7]
+	fmt.Println(cap(pins)) //10
+	fmt.Println(len(pins)) //8
+
+	//direct declare
+	allNums := []int{}
+	allNums = append(allNums, 2)
+	fmt.Println(allNums)      //[2]
+	fmt.Println(cap(allNums)) //1
+
+	//adding elements using index
+	var mobile = make([]int, 2, 5)
+
+	mobile[0] = 34
+	mobile[1] = 2
+	fmt.Println(mobile) // [34 2]
+
+	//copy function slice
+
+	var studentsRollNumber = make([]int, 0, 5)
+	studentsRollNumber = append(studentsRollNumber, 2)
+	var studentsRollNumber2 = make([]int, len(studentsRollNumber))
+
+	copy(studentsRollNumber2, studentsRollNumber)
+	fmt.Println(studentsRollNumber, studentsRollNumber2) //[2] [2]
+
+	//slice operator
+	var allDigits = []int{1, 2, 3}
+
+	fmt.Println(allDigits[0:2]) // [1,2]
+	fmt.Println(allDigits[:1])  //[1]
+	fmt.Println(allDigits[2:])  // [3]
+
+	//slice package
+
+	var nums1 = []int{1, 2}
+	var nums2 = []int{1, 2}
+	var nums3 = []int{1, 2, 3}
+
+	fmt.Println(slices.Equal(nums1, nums2)) //true
+	fmt.Println(slices.Equal(nums2, nums3)) //false
+
+	//2d slices
+
+	var nums4 = [][]int{{1, 2, 3}, {4, 5, 6}}
+	fmt.Println(nums4) //[[1 2 3] [4 5 6]]
+
+}
