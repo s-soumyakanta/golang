4b26b0f3ca8256403d82d13c3e0f538336af4088
S Soumyakanta <mr.ssoumyakanta@gmail.com>
2024-08-18 20:57:46 +0530

    maps and range

---
+++ learn/10_maps/maps.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"maps"
+)
+
+func main() {
+	//creating map - similar to object in JavaScript
+
+	m := make(map[string]string)
+
+	//setting and element
+
+	m["name"] = "S Soumyakanta"
+	m["language"] = "JavaScript"
+
+	//get an element
+
+	fmt.Println(m["name"], m["language"]) //S Soumyakanta JavaScript
+
+	// while accessing non existing key
+	// Imp: if key does not exists in the map then it returns zero value
+	fmt.Println(m["phone"]) // we are getting empty string
+
+	marks := make(map[string]int)
+
+	marks["Math"] = 30
+	marks["Science"] = 90
+
+	fmt.Println(marks)      //map[Math:30 Science:90]
+	fmt.Println(len(marks)) //2
+
+	delete(marks, "Science")
+
+	fmt.Println(len(marks)) //1
+
+	clear(marks) // empty now
+
+	//making maps without using make - if we know elements in advance
+	phNums := map[string]int{"Soumya": 123, "SS": 454}
+
+	fmt.Println(phNums) //map[SS:454 Soumya:123]
+
+	//getting element in go
+	k, ok := phNums["SS"]
+	fmt.Println(k) //454
+	if ok {
+		fmt.Println("All ok")
+	} else {
+		fmt.Println("Not ok")
+	}
+
+	map1 := map[string]int{"price": 44, "phones": 3}
+	map2 := map[string]int{"price": 44, "phones": 3}
+
+	//check whethere two maps are equal or not
+
+	fmt.Println(maps.Equal(map1, map2)) //true
+
+}
---
+++ learn/11_range/range.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//we use range to iterating over data structures
+
+	numbers := []int{4, 2, 4}
+
+	for i := 0; i < len(numbers); i++ {
+		fmt.Println(numbers[i]) // 4, 2, 4
+	}
+
+	//for loop with range
+
+	sum := 0
+	for ind, numbers := range numbers {
+		fmt.Println(numbers) // 4, 2, 4
+
+		sum = sum + numbers
+
+		fmt.Println(numbers, ind) // 4 0 , 2 1, 4 2
+	}
+
+	fmt.Println(sum) // 10
+
+	customers := map[string]string{"fName": "S", "lName": "Soumyakanta"}
+
+	for k, v := range customers {
+		fmt.Println(k, v) //fName S lName Soumyakanta
+	}
+
+	//using range over strings
+
+	for i, c := range "golang" {
+		fmt.Println(i, c)
+		/* 0 103
+		1 111
+		2 108
+		3 97
+		4 110
+		5 103
+		*/
+		// unicode code point rune
+		// above the i is the starting byte of rune
+		/* Multi-line comments start with /* and ends with */
+
+		fmt.Println(i, string(c))
+		/*0 g
+		1 o
+		2 l
+		3 a
+		4 n
+		5 g */
+	}
+
+}
