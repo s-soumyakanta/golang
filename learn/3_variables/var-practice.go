33cb6df9532b4e8e8edefce26fcf3333e067d69d
S Soumyakanta <mr.ssoumyakanta@gmail.com>
2025-01-05 06:02:53 +0530

    add variable practice

---
+++ learn/3_variables/var-practice.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var studentName string = "S Soumyakanta"
+	fmt.Println(studentName)
+
+	//no need to define type
+
+	var studentMarksInEnglish = 50
+	fmt.Println(studentMarksInEnglish)
+
+	//shorthand for variable
+	studentAge := 33
+	fmt.Println(studentAge)
+
+	var studentSection string
+	studentSection = "B"
+	fmt.Println(studentSection)
+}
--- learn/3_variables/var.go
+++ learn/3_variables/var.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+// We have to use all declaired variables in golang
+
 func main() {
 	var name string = "S Soumykanta"
 	var langName = "golang"
@@ -12,6 +14,9 @@ func main() {
 	myName := "Soumya"
 
 	//If we are only defining the var then we need to specify its type
+	/* If we are declaring a variable in which the value will be assigned
+	later then we have to declare the type of the variable at the time of initiation */
+
 	var myNum int
 	myNum = 22
 
