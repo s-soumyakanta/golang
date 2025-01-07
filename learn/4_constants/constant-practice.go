a53f1cadc8d944411f2df1df6f2f569eb74e0fd5
S Soumyakanta <mr.ssoumyakanta@gmail.com>
2025-01-07 08:58:07 +0530

    add constant practice

--- learn/4_constants/constant-practice.go
+++ learn/4_constants/constant-practice.go
@@ -7,8 +7,18 @@ const (
 	mobile = 2233993377
 )
 
+const (
+	serverName = "ec2One"
+)
+
+const brandName = "Holla"
+
+//shorthands not allowed outside
+// dd := 33
 const age int = 22
 
 func main() {
 	fmt.Println(name, mobile, age)
+	fmt.Println(brandName)
+	fmt.Println(serverName)
 }
