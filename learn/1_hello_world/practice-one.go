38e4c80d590e860a016c7f024f9bf41225835da9
S Soumyakanta <mr.ssoumyakanta@gmail.com>
2025-01-10 08:45:15 +0530

    add go revision

--- learn/1_hello_world/practice-one.go
+++ learn/1_hello_world/practice-one.go
@@ -2,6 +2,26 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 	fmt.Println("Practice One")
+	var a, b, c int = 1, 2, 3
+	fmt.Println(a, b, c)
+	const e, f, g = true, false, "no!"
+	fmt.Println(e, f, g)
+
+	const (
+		age     = 22
+		name    = "Soumya"
+		country = "India"
+	)
+
+	if age >= 18 || country == "India" {
+		fmt.Println(name, "is eligible to vote")
+	} else {
+		fmt.Println(name, "is not eligible to vote")
+	}
+
+	for i := 0; i < 5; i++ {
+		fmt.Println(i + 10)
+	}
 }
