2b4a6fa10fa302d742fb3b49dc5c9e04928afb45
S Soumyakanta <mr.ssoumyakanta@gmail.com>
2025-08-30 11:20:41 +0530

    add values and variables practice

--- learn/2_simple_values/values-practice-one.go
+++ learn/2_simple_values/values-practice-one.go
@@ -1,17 +1,32 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	//String
+// 	fmt.Println("Soumya")
+
+// 	//Bool
+// 	fmt.Println(true)
+
+// 	//Intiger
+// 	fmt.Println(33)
+
+// 	//Float
+// 	fmt.Println(33.22)
+// }
+
 package main
 
 import "fmt"
 
 func main() {
-	//String
 	fmt.Println("Soumya")
+	// This is a comment
+	/* This is a
+	multiline comment */
 
-	//Bool
-	fmt.Println(true)
-
-	//Intiger
-	fmt.Println(33)
-
-	//Float
-	fmt.Println(33.22)
+	fmt.Println(true)  //boolean
+	fmt.Println(22.33) //float
+	fmt.Println(44)    //Intiger
 }
--- learn/3_variables/var-practice.go
+++ learn/3_variables/var-practice.go
@@ -1,21 +1,33 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var studentName string = "S Soumyakanta"
+// 	fmt.Println(studentName)
+
+// 	//no need to define type
+
+// 	var studentMarksInEnglish = 50
+// 	fmt.Println(studentMarksInEnglish)
+
+// 	//shorthand for variable
+// 	studentAge := 33
+// 	fmt.Println(studentAge)
+
+// 	var studentSection string
+// 	studentSection = "B"
+// 	fmt.Println(studentSection)
+// }
+
 package main
 
 import "fmt"
 
 func main() {
-	var studentName string = "S Soumyakanta"
-	fmt.Println(studentName)
-
-	//no need to define type
-
-	var studentMarksInEnglish = 50
-	fmt.Println(studentMarksInEnglish)
-
-	//shorthand for variable
-	studentAge := 33
-	fmt.Println(studentAge)
-
-	var studentSection string
-	studentSection = "B"
-	fmt.Println(studentSection)
+	var name string
+	age := 99
+	name = "Soumya"
+	mob := 88
+	fmt.Println(name, age, mob)
 }
