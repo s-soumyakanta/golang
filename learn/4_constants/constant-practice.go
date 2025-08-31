4aa18120cb1c30706f6ab2dc87b9bd56c14dbde0
S Soumyakanta <mr.ssoumyakanta@gmail.com>
2025-08-31 16:56:32 +0530

    add constant practice in golang

--- learn/4_constants/constant-practice.go
+++ learn/4_constants/constant-practice.go
@@ -1,24 +1,38 @@
+// package main
+
+// import "fmt"
+
+// const (
+// 	name   = "S Soumyakanta"
+// 	mobile = 2233993377
+// )
+
+// const (
+// 	serverName = "ec2One"
+// )
+
+// const brandName = "Holla"
+
+// //shorthands not allowed outside
+// // dd := 33
+// const age int = 22
+
+// func main() {
+// 	fmt.Println(name, mobile, age)
+// 	fmt.Println(brandName)
+// 	fmt.Println(serverName)
+// }
+
 package main
 
 import "fmt"
 
-const (
-	name   = "S Soumyakanta"
-	mobile = 2233993377
-)
-
-const (
-	serverName = "ec2One"
-)
-
-const brandName = "Holla"
-
-//shorthands not allowed outside
-// dd := 33
-const age int = 22
+const PI float32 = 3.14
 
 func main() {
-	fmt.Println(name, mobile, age)
-	fmt.Println(brandName)
-	fmt.Println(serverName)
+	const (
+		AGE   int = 20
+		STATE     = "Odisha"
+	)
+	fmt.Println(PI, AGE, STATE)
 }
