e1d5819c73862c080613b8bd2ca1e8281aa08301
S Soumyakanta <mr.ssoumyakanta@gmail.com>
2025-08-31 21:58:46 +0530

    add const practice in golang

--- learn/4_constants/constant-practice.go
+++ learn/4_constants/constant-practice.go
@@ -34,5 +34,11 @@ func main() {
 		AGE   int = 20
 		STATE     = "Odisha"
 	)
-	fmt.Println(PI, AGE, STATE)
+	fmt.Printf("AGE has value %v and type: %t", AGE, STATE)
+	//Always match the verb (%d, %s, %f, %T, etc.) with the type of the variable.
+	fmt.Printf("AGE has value %v and type: %T\n", AGE, AGE)
+	fmt.Printf("STATE has value %v and type: %T\n", STATE, STATE)
+	fmt.Printf("PI has value %v and type: %T\n", PI, PI)
+
+	fmt.Println(PI, AGE, "\n", STATE)
 }
