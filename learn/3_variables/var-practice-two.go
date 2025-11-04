ef747153740a48ad311d35a2c4ade8de634ad513
S Soumyakanta <mr.ssoumyakanta@gmail.com>
2025-11-04 12:36:43 +0530

    add var practice

---
+++ learn/3_variables/var-practice-two.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello")
+	fmt.Println(4.4 + 4)
+	fmt.Println(true)
+
+	ageOne := 4
+
+	var ageTwo int = 9
+
+	var ageSum int
+	ageSum = ageOne + ageTwo
+
+	fmt.Println(ageSum)
+}
